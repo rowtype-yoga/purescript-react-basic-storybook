cf3b24ab6b9dc3bb9ac399b6c0dd7a1bccbe6e60
Mark Eibes <mark.eibes@gmail.com>
2022-11-06 20:54:43 +0100

    Delete AppDecorator.js

--- example/stories/AppDecorator.js
+++
@@ -1,4 +0,0 @@
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import purescript from 'react-syntax-highlighter/dist/esm/languages/prism/purescript';
-
-export const registerPS = () => SyntaxHighlighter.registerLanguage("purescript", purescript)
